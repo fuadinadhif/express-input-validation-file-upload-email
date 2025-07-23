1b157929a3b3b0598b2b834a1031956b71932449
fuadinadhif <nadhiffuadi@gmail.com>
2025-07-23 15:57:00 +0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+
+import authRoutes from "./routes/auth.route.js";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    console.error(error);
+
+    response
+      .status(500)
+      .json({ message: error.message || "Unknown error", error });
+  }
+);
+
+const PORT: string = "8000";
+app.listen(PORT, () => console.info(`Server is listening on port: ${PORT}`));
